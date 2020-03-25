edd3cbbfeadbc6f042bd02a2c21123c5b6fe577a
Florent.Bourgeois <florent.bourgeois@uha.fr>
2020-03-25 13:53:28 +0100

    initial commit

---
+++ src/main/java/fr/CCI/LPDAOO/java/paint_Back/PaintBackApplication.java
@@ -0,0 +1,13 @@
+package fr.CCI.LPDAOO.java.paint_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/CCI/LPDAOO/java/paint_Back/PaintBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.CCI.LPDAOO.java.paint_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
